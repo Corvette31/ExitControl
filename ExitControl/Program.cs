e278420c580a6f51b7a5515bb05084fa4badc30e
Corvette31 <corvette.ss@yandex.ru>
2022-06-15 02:47:20 +0300

    Добавьте файлы проекта.

---
+++ ExitControl/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExitControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            while (userInput != "exit")
+            {
+                Console.Write("Я буду выводить это сообщения, пока вы не напишите слово exit.\nВведите слово : ");
+                userInput = Console.ReadLine();
+            }
+            Console.Write("Выхожу...");
+        }
+    }
+}
